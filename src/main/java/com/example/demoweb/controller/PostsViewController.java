84b38f0101eb51154cd27e61a733aabec1fc591b
Blushev <egor.rumyancev.9494@mail.ru>
2023-02-18 13:19:26 +0500

    link Bootstrap

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -1,6 +1,5 @@
 package com.example.demoweb.controller;
 
-
 import com.example.demoweb.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 @Controller
 public class PostsViewController {
     @Autowired
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -4,16 +4,24 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
 @Service
 public class PostService {
     public List<Post> listAllPosts(){
+        Date date1 = new Date();
+        Date date2 = new Date();
+        Date date3 = new Date();
+        date1.getTime();
+        date2.getTime();
+        date3.getTime();
+
         ArrayList<Post> list = new ArrayList<Post>();
-        list.add(new Post("Пост№1", 12));
-        list.add(new Post("Пост№2", 10));
-        list.add(new Post("Пост№3", 9));
+        list.add(new Post("Пост№1", 12, date1));
+        list.add(new Post("Пост№2", 10, date2));
+        list.add(new Post("Пост№3", 9, date3));
 
         return list;
     }
